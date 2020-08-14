676632ca27f0d050da5c7ad3911f4eaaa1f6e212
gbkkk <ogabrielsldss@gmail.com>
2020-08-14 19:23:07 -0300

    Terceiro comando

---
+++ commands/entretenimento/abraçar.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js')
+const superagent = require('superagent')
+
+module.exports = {
+    nome: "abraçar",
+    alternativas: ["abraçar", "hug"],
+    run: async(client, message, args, owner) => {
+
+        const membro = message.mentions.users.first() || message.guild.members.cache.get(args[0])
+        if(!membro) return message.reply(`Eu não consegui encontrar o membro que você inseriu...`)
+
+        if(membro.id === message.author.id) return message.reply(`Você não pode se abraçar bobo(a) :pensive:`)
+
+        let { body } = await superagent.get('https://nekos.life/api/hug');
+
+        var embed = {
+            title: `<:abraco:695368827612954715> ⋅ ${message.author.username} beijou ${membro.username}`,
+            color: "AQUA",
+            image: {
+                url: body.url
+            }
+        }
+
+        let msg = await message.reply({ embed: embed })
+        msg.react("695368827612954715")
+
+
+    }
+}
