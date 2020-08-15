2eb9243cb89e347ac871429677ba13623011cf94
gbkkk <ogabrielsldss@gmail.com>
2020-08-15 16:43:06 -0300

    Sexto comando

---
+++ commands/entretenimento/beijar.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js')
+const superagent = require('superagent')
+
+module.exports = {
+    nome: "beijar",
+    alternativas: ["kiss", "beijar"],
+    run: async(client, message, args, owner) => {
+
+        const membro = message.mentions.users.first() || message.guild.members.cache.get(args[0])
+        if(!membro) return message.reply(`Eu não consegui encontrar o membro que você inseriu...`)
+
+        if(membro.id === message.author.id) return message.reply(`KKK? Você não pode se beijar bobo(a).`)
+
+        let { body } = await superagent.get('https://nekos.life/api/kiss');
+
+        var embed = {
+            title: `😍 ⋅ ${message.author.username} beijou ${membro.username}`,
+            color: "AQUA",
+            image: {
+                url: body.url
+            }
+        }
+        let msg = await message.reply({ embed: embed })
+
+        msg.react("😍")
+
+    }
+}
