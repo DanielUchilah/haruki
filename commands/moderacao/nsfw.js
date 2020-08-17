ec4837cfa8e3d01b32ae1899605b32b8de320925
gbkkk <ogabrielsldss@gmail.com>
2020-08-17 17:42:21 -0300

    Vingesimo terceiro comando

---
+++ commands/moderacao/nsfw.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    nome: "nsfw",
+    alternativas: ["nsfw"],
+    run: async(client, message, args) => {
+
+        if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply("Você não pode setar NSFW nesse canal boba(o). A permissão necessária é **Gerenciar canais.**")
+
+        message.channel.setNSFW(true, "Foi executado o comando de NSFW")
+        message.reply("<a:rsrs:744303388535029871> ⋅ Canal atualizado.")
+
+    }
+}
