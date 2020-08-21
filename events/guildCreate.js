ee281f6f7de69a1a79ea4afae770c4fbeb0e45ab
gbkkk <ogabrielsldss@gmail.com>
2020-08-21 16:34:58 -0300

    Log 1

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,5 +4,5 @@ module.exports = (client, guild) => {
 
     let canal = client.channels.cache.get("744692043904843806")
     canal.send(`Fui adicionado no servidor ${guild.name} que possue o ${guild.owner}/${guild.owner.id} como dono.`)
-    
+
 }
